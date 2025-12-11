04f074ff2986f73732e65c525201f491a2a082f8
atyrrel33 <atyrrel33@gmail.com>
2025-12-11 16:45:22 -0500

    Update chronicle-core.js

--- apps/chronicle/chronicle-core.js
+++ apps/chronicle/chronicle-core.js
@@ -71,6 +71,9 @@ function initializeApp() {
     // Setup all event listeners
     setupEventListeners();
     
+      // ✨ ADD THIS SINGLE LINE:
+    if (typeof ChronicleDesk !== 'undefined') ChronicleDesk.init();
+    
     // Detect and handle session
     detectSession();
     
